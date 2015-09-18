381d677ad0259a176720c7a7cc165c4c7dba9af5
Rob Moffat <robmoffat@mac.com>
2015-09-18 13:39:52 +0100

    JUST BEFORE THE NEW SEMANTICS.

--- pure4j-checker/src/test/java/org/pure4j/checker/basic/pure/setting_fields/FieldSetting.java
+++ pure4j-checker/src/test/java/org/pure4j/checker/basic/pure/setting_fields/FieldSetting.java
@@ -1,6 +1,9 @@
 package org.pure4j.checker.basic.pure.setting_fields;
 
 import org.pure4j.annotations.pure.Pure;
+import org.pure4j.checker.basic.support.CausesError;
+import org.pure4j.checker.basic.support.ShouldBePure;
+import org.pure4j.exception.PureMethodAccessesSharedFieldException;
 
 @Pure
 public class FieldSetting {
@@ -8,13 +11,17 @@ public class FieldSetting {
 	public int x = 0;
 	private int y = 0;
 	
+	@CausesError(PureMethodAccessesSharedFieldException.class)
+	public FieldSetting() {
+	}
 	
+	@CausesError(PureMethodAccessesSharedFieldException.class)
 	public void increment() {
-		x++;
-		y++;
+		y = x+1;
 	}
 	
 	@Pure
+	@ShouldBePure
 	public static FieldSetting smuggleSomeMutableState() {
 		return new FieldSetting();
 	}
